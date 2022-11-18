1c2bee78a9759f52ee2a5efaa2abb4a0ae054610
Todd Pierzina <todd.pierzina@collectivehealth.com>
2022-11-18 17:33:56 -0600

    snapshot of mini15 pre-air22

--- dot_bashrc.sh
+++ dot_bashrc.sh
@@ -7,7 +7,7 @@
 
 # If debugging is not enabled, overwrite __echo with a no-op.
 if [[ -e ~/.tick.enabled ]]; then
-  __echo() { echo "$@"; }
+  __echo() { echo $@; }
 else
   __echo() { :; }
 fi
@@ -26,16 +26,320 @@ export PREFS="$HOME/prefs"
 
 #? export SUBLIME_PACKAGES="$HOME"'/Library/Application Support/Sublime Text 3/Packages'
 
-
 # Put my homemade scripts and other miscellany here at the start of the classpath.
 [[ -d "$HOME/bin" && ! "$PATH" =~ $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
 
+#!!!!!
+#
+
+### echo helpers
+#   v* = never print unless SH_VERBOSE is set
+#   q* = always print unless SH_QUIET is set
+#   e* = always print but also to error
+echo-verbose()  { ((SH_VERBOSE)) || return 0; echo $@; }
+echo-quiet()    { ((SH_QUIET)) && return 0; echo $@; }
+echo-error()    { >&2 echo $@; }
+vecho(){ echo-verbose $@; }
+qecho(){ echo-quiet $@; }
+eecho(){ echo-error $@; }
+#
+# List all variables matching $1 (globbing *, etc.) and their values.
+echo-glob() {
+  [[ -z "$1" ]] && echo-error "usage: echo-glob patt [...]" && return 1
+  for patt in $@; do
+    [[ ! "$patt" =~ [*?]$ ]] && patt="${patt}*"
+    local IFS=' '; for var in $(eval echo $(printf "\${!%s}" "$patt")); do
+      printf "%s=%s\n" "$var" "${!var}"
+    done
+  done
+}
+gecho(){ echo-glob $@; }
+#
+# Replace newlines, carriage-returns and tabs with \n, \r and \t.
+echo-unescape() {
+  if [[ ! -t 0 ]]; then
+    sed -E -n 'l;' \
+    | join-lines '\\n' \
+    | sed -E 's/\$(\\n)/\1/g; s/\$$//g;'
+    return 0;
+  fi
+  [[ -z "$1" ]] && eecho 'usage: echo-unescape text [...] or echo-unescape <<< text' && return 1
+  echo-unescape <<< $@
+}
+uecho(){ echo-unescape $@; }
+
+### printf helpers
+#
+printf-verbose()  { ((SH_VERBOSE)) || return 0; printf $@; }
+printf-quiet()    { ((SH_QUIET)) && return 0; printf $@; }
+printf-error()    { >&2 printf $@; }
+vprintf(){ printf-verbose $@; }
+qprintf(){ printf-quiet $@; }
+eprintf(){ printf-error $@; }
+
+### eval helpers
+#
+# Conditionally echo the expression to stderr before executing it, using similar logic as echo-* and printf-*.
+: ${EVAL_ECHO_PREFIX:=\>$}
+eval-echo()     { >&2 echo "$EVAL_ECHO_PREFIX" $@; eval $@; }
+eval-verbose()  { >&2 echo-verbose "$EVAL_ECHO_PREFIX" $@; eval $@; }
+eval-quiet()    { >&2 echo-quiet "$EVAL_ECHO_PREFIX" $@; eval $@; }
+eval-error()    { >&2 echo-error "$EVAL_ECHO_PREFIX" $@; eval $@; }
+veval(){ eval-verbose $@; }
+qeval(){ eval-quiet $@; }
+eeval(){ eval-error $@; }
+#
+# Always ECHO the given expression; but do not EVAL if SH_WHATIF is set.
+: ${EVAL_WHATIF_PREFIX:=#$}
+eval-whatif()   { ((SH_WHATIF)) && echo "$EVAL_WHATIF_PREFIX" $@ || eval-echo $@; }
+weval() { eval-whatif $@; }
+
+### find helpers
+#
+nfind(){ eval-quiet find -L . -name $@; }
+pfind(){ eval-quiet find -L . -path $@; }
+rfind(){ eval-quiet find -L -E . -regex $@; }
+
+### ls helpers
+#
+# -A  show .* files except for '.'' and '..'
+# -d  list directories as plain files, not recursed
+#
+# -H  follow only symlink arguments
+# -L  follow all symlinks
+# -P  follow no symlinks
+#
+# -1  ("one") 1-column output
+# -l  ("el") use long form, show owner and group
+# -g  use long form, suppress owner
+# -o  use long form, suppress group
+# -h  use human-readable file sizes
+# -F  add file suffix [/, @, *, ...]
+#
+# -tr sort by time modified, old to new
+# -Sr sort by size, ascending
+#
+l1()    { ls -1F $@; }
+l1r()   { l1 -r $@; }
+#
+ll()    { ls -oghF $@ | tilde-compress; }
+llt()   { ll -t $@; }
+lltr()  { ll -tr $@; }
+lls()   { ll -S $@; }
+llsr()  { ll -Sr $@; }
+#
+la()    { ll -A $@; }
+lat()   { la -t $@; }
+latr()  { la -tr $@; }
+las()   { la -S $@; }
+lasr()  { la -Sr $@; }
+#
+l1d()  { l1 -d $@; }
+lad()  { la -d $@; }
+lad()  { la -d $@; }
+#
+# Think "ll and la but narrower": cut out permissions, link count and owner.
+#   $ ls -ohF
+#   total 520
+#   -rw-r--r--  1 toddpierzina   1.2K Mar 10 11:33 README.md
+# $1: permissions, $2: inode count, $3: owner, $4: size, $5: date/time, $6: filename
+lln() {
+  ls -oF $@ \
+  | sed -E \
+    -e '/^total .+$/d' \
+    -e 's/^([^ ]{9,}) +([[:digit:]]+) +([^ ]+) +([^ ]+) ([^ ]+ +[^ ]+ +[^ ]+) +(.+)$/\4'$'\t''\5'$'\t''\6/' \
+    -e "s:\\$HOME:\\~:g" \
+  | awk -F$'\t' \
+    '{printf "%12'$'\'''d  %s  %s\n", $1, $2, $3}'
+}
+lan() { lln -A $@; }
+#
+# Display permissions in octal, from: http://askubuntu.com/a/152005
+# I've tried to figure out how this works but have no fucking clue.
+lso() {
+  ls -ohF $@ \
+  | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print}' \
+  | sed -E -e "s:\\$HOME:\\~:g"
+}
+
+### 'mv' helpers
+#
+alias mvv='mv -v'
+
+### 'rm' helpers
+#
+alias rmv='rm -v'
+#
+# Delete the target of a symlink, then the symlink.
+rmln() {
+  local cmd="rm"
+  while [[ "$1" =~ ^- ]]; do cmd="$cmd $1" && shift; done
+
+  local link="$1" && shift
+  [[ -z "$link" ]] && eecho "usage: rmln [rm opts] symlink" && return 1
+  [[ ! -L "$link" ]] && eecho "rmln: $link: no such symlink" && return 1
+
+  local dest="$(readlink "$link")"
+  [[ ! -e "$dest" ]] && eecho "rmln: $link -> $dest: no such file or directory" && return 1
+  qeval "$cmd '$dest'" || return 1
+}
+
+### 'tail' helpers
+#
+alias t='tail'
+alias tf='tail -f'
+
+### path helpers
+#
+# Add given path element to the beginning of the variable, unless already present.
+# usage: path-prepend [var] path
+path-prepend() {
+  local var='PATH'; [[ -n "$2" ]] && var="$1" && shift
+  local val="${!var}"
+  [[ ! "$val" =~ (^|:)$1(:|$) ]] && export $var="$1:$val"
+}
+#
+# List path variables' elements, 1 per line.
+path-list() {
+  ((! $#)) && echo-error 'usage: path-list PATHVAR [...]' && return 1
+  local var=${1:-PATH}
+  split-lines ':' <<< "${!var}" | tilde-compress
+}
+alias pecho=path-list path-echo=path-list
+
+
+# The following functions operate on stdin OR $@; [[ -t 0 ]] is true if stdin is a terminal
+# from: https://stackoverflow.com/a/30520299
+#
+# Compress '~' to value of $HOME, or vice versa.
+tilde-compress() {
+  [[ ! -t 0 ]] && sed -E -e "s:\\$HOME:\\~:g" && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: tilde-compress path [...], or ... | tilde-compress" && return 1
+  tilde-compress <<< $@
+}
+tilde-expand() {
+  [[ ! -t 0 ]] && sed -E -e "s:\\~:$HOME:g" && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: tilde-expand path [...], or ... | tilde-expand" && return 1
+  tilde-expand <<< $@
+}
+#
+trim() {
+  [[ ! -t 0 ]] && sed -E -e 's/[[:space:]]*(.*)[[:space:]]*/\1/g' && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: trim str [...], or ... | trim" && return 1
+  trim <<< $@
+}
+rtrim() {
+  [[ ! -t 0 ]] && sed -E -e 's/(.*)[[:space:]]*/\1/g' && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: rtrim str [...], or ... | rtrim" && return 1
+  rtrim <<< $@
+}
+ltrim() {
+  [[ ! -t 0 ]] && sed -E -e 's/[[:space:]]*(.*)/\1/g' && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: ltrim str [...], or ... | ltrim" && return 1
+  ltrim <<< $@
+}
+#
+upper() {
+  [[ ! -t 0 ]] && tr '[:lower:]' '[:upper:]' && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: upper word [...], or ... | upper" && return 1
+  upper <<< $@
+}
+lower() {
+  [[ ! -t 0 ]] && tr '[:upper:]' '[:lower:]' && return 0
+  [[ -z "$1" ]] && >&2 echo "usage: lower word [...], or ... | lower" && return 1
+  lower <<< $@
+}
+
+# Concatenate trimmed lines from stdin onto a single line, delimited by $1 [, ]
+join-lines() {
+  local delim="${1:-, }" && shift
+  local not_1st=
+  sed -E -e 's/^ *(.+) *$/\1/;' \
+  | while read -r line; do
+      ((not_1st)) && printf "$delim" || not_1st=1
+      printf "%s" "$line"
+    done \
+  && printf '\n'
+}
+# Split line(s) from stdin into separate lines, using $1 [,] as delimiter
+split-lines() {
+    local delim="${1:-,}"
+    sed -E -e "s/([^$delim]*)$delim([^$delim]*)/\\1"\\$'\n'"\\2/g"
+}
+
+# usage: [ms places] [format]
+datetime-plus-ms() {
+  local places="${1:-3}" && shift
+  local format="${1:-%D %T}" && shift
+  local ms="$(perl - <<-'EOF'
+    use Time::HiRes qw(time);
+    my $t = time;
+    printf "%06d", ($t - int($t)) * 1000000;
+  EOF
+  )00000"
+  date +"$format.${ms:0:$places}"
+}
+
+datetime-epoch-ms() {
+  echo "$(perl - <<-'EOF'
+    use Time::HiRes qw(time);
+    printf "%d", time * 1000;
+  EOF
+  )"
+}
+
+safe-source() {
+  local opt_quiet=; [[ "$1" =~ ^(-q|--quiet) ]] && opt_quiet=1 && shift
+  local script_path="$1"; shift
+  [[ -z "$script_path" ]] && echo-error "usage: safe-source script" && return 1
+  if [[ ! -e "$script_path" ]]; then
+    ((! opt_quiet)) && echo-error "safe-source: $script_path: No such file"
+    return 1
+  fi
+  . "$script_path"
+}
+
+.tidy_system_path() {
+  local old_path="$PATH"
+  # echo "PATH_0:"
+  # path-list PATH
+
+  # Homebrew expects sbin to be in the path.
+  path-prepend PATH "/usr/local/sbin"
+  # echo "PATH_1: /usr/local/sbin"
+  # path-list PATH
+
+  # Put Ruby 3 in front of system's 2.6.
+  path-prepend PATH "/usr/local/opt/ruby/bin"
+  # echo "PATH_2: /usr/local/opt/ruby/bin"
+  # path-list PATH
+
+  # Put my homemade scripts and other miscellany here at the start of the classpath.
+  path-prepend PATH "/usr/local/opt/gnu-tar/libexec/gnubin"
+  path-prepend PATH "$HOME/bin"
+  path-prepend PATH "$HOME"
+  # echo "PATH_3: HOME, HOME/bin, /usr/local/opt/gnu-tar/libexec/gnubin"
+  # path-list PATH
+
+  [[ "$old_path" == "$PATH" ]] && return 0
+
+  .tick-bashrc ".tidy_system_path: PATH updated from:"
+  local IFS=:; for p in $old_path; do .tick-bashrc ": $p"; done
+  .tick-bashrc ".tidy_system_path: PATH updated to:"
+  local IFS=:; for p in $PATH; do .tick-bashrc ": $p"; done
+}
+#? .tidy_system_path
+#
+#!!!!!
+
+####
+#
 
 # error, info, verbose and debug levels; uses SH_ vars which can be set pre-execution or generally via -q, -v and -d
-echo-info() { ((SH_QUIET)) || echo "$@"; return 0; }
-echo-verbose() { ((SH_VERBOSE || SH_DEBUG)) && echo "$@"; return 0; }
-echo-debug() { ((SH_DEBUG)) && echo "$@"; return 0; }
-echo-error() { >&2 echo "$@"; return 0; }  # to stderr
+echo-info() { ((SH_QUIET)) || echo $@; return 0; }
+echo-verbose() { ((SH_VERBOSE || SH_DEBUG)) && echo $@; return 0; }
+echo-debug() { ((SH_DEBUG)) && echo $@; return 0; }
+echo-error() { >&2 echo $@; return 0; }  # to error
 alias iecho=echo-info vecho=echo-verbose decho=echo-debug eecho=echo-error
 
 echo-eval()  {
@@ -44,10 +348,10 @@ echo-eval()  {
   $echo_level '>$ '$*
   eval $*
 }
-eecho-eval() { echo-eval --echo eecho "$@"; }
-iecho-eval() { echo-eval --echo iecho "$@"; }
-vecho-eval() { echo-eval --echo vecho "$@"; }
-decho-eval() { echo-eval --echo decho "$@"; }
+eecho-eval() { echo-eval --echo eecho $@; }
+iecho-eval() { echo-eval --echo iecho $@; }
+vecho-eval() { echo-eval --echo vecho $@; }
+decho-eval() { echo-eval --echo decho $@; }
 
 # "What-if" echo: if SH_WHATIF env var is set, simply echo the given command; else ieval it.
 wecho-eval() {
@@ -89,8 +393,8 @@ parse-bool() {
 join-array() {
     local delim="$1" && shift
     local i_first=1
-    [[ -n "$SH_DEBUG" ]] && echo_vars delim i_first "$@"
-    for i in "$@"; do
+    [[ -n "$SH_DEBUG" ]] && echo_vars delim i_first $@
+    for i in $@; do
         [[ -n "$SH_VERBOSE" ]] && eecho "i=$i"
         [[ -n "$i_first" ]] && printf "%s" "$i" && unset i_first || printf "%s%s" "$delim" "$i"
     done
@@ -99,9 +403,9 @@ join-array() {
 
 uniq-array() {
     local i_first=1
-    [[ -n "$SH_DEBUG" ]] && eecho_vars delim i_first "$@"
+    [[ -n "$SH_DEBUG" ]] && eecho_vars delim i_first $@
     local buff=
-    for i in "$@"; do
+    for i in $@; do
         [[ -n "$SH_VERBOSE" ]] && eecho "i=$i"
         if (( i_first )); then
             buff="$i"
@@ -122,21 +426,21 @@ join-lines() {
 # Expand '~' to value of $HOME, or compress value of $HOME to ~
 tilde-compress() {
   [[ -z "$1" ]] && eecho "usage: tilde-compress path [...]" && return 1
-  tilde-home-compress-expand 'tilde-compress' '${path/$HOME/\~}' "$@"
+  tilde-home-compress-expand 'tilde-compress' '${path/$HOME/\~}' $@
 }
 tilde-expand() {
   [[ -z "$1" ]] && eecho "usage: tilde-expand path [...]" && return 1
-  tilde-home-compress-expand 'tilde-expand' '${path/\~/$HOME}' "$@"
+  tilde-home-compress-expand 'tilde-expand' '${path/\~/$HOME}' $@
 }
 #
 # Compress user's home folder to the literal string '$HOME' (for writing commands to a script file, generally)
 home-compress() {
   [[ -z "$1" ]] && eecho "usage: home-compress path [...]" && return 1
-  tilde-home-compress-expand 'home-compress' '${path/$HOME/\$HOME}' "$@"
+  tilde-home-compress-expand 'home-compress' '${path/$HOME/\$HOME}' $@
 }
 home-expand() {
   [[ -z "$1" ]] && eecho "usage: home-expand path [...]" && return 1
-  tilde-home-compress-expand 'home-expand' '${path/\$HOME/$HOME}' "$@"
+  tilde-home-compress-expand 'home-expand' '${path/\$HOME/$HOME}' $@
 }
 #
 tilde-home-compress-expand() {
@@ -159,6 +463,8 @@ safe-source-script() {
   [[ ! -e "$script_file" ]] && vecho "safe-source-script: '$script_file': not found; skipping" && return 0
   . "$script_file"
 }
+#
+###
 
 alias .reload-bashrc=". $HOME/.bashrc"
 alias .rlbrc='.reload-bashrc'
--- dot_tick.sh
+++ dot_tick.sh
@@ -1,90 +1,92 @@
 #!/usr/bin/env bash
 
-# If .do-tick is true, .tick logs to ~/.tick.log.
+# If .do-tick() is true, .tick logs to ~/.tick.log.
 # Then it also echoes to stdout/stderr if TICK_STDOUT/TICK_STDERR is set.
 # Optional .tick options:
 #   --scriptname  used with '.tick' to determine whether this script's ticks should fire
 #                 (if omitted then ignore this check)
 #   --eval        evaluate expression before echoing it (good for potentially expensive messages)
 #   --vars        log each variable given along with its value
-.tick-wrapper() {
 
-  # Return true (0) if ~/.tick.ALL or .tick$sname exists. E.g., .tick.bashrc.toddpierzina
-  .do-tick() {
-    [[ -e ~/.tick.ALL ]] && return 0
-    [[ -z "$1" ]] && return 1
-    [[ -e ~/.tick$1 ]]
-  }
-
-  .tick() {
-    local sname= opt_eval= opt_vars=
-    while [[ "$1" =~ ^- ]]; do case "$1" in
-      -s|--script) sname="$2"; shift 2;;
-      -e|--eval) opt_eval=1; shift;;
-      -v|--vars) opt_vars=1; shift;;
-      *) >&2 echo ".tick: $1: invalid option" && return 1;;
-    esac; done
-    .do-tick "$sname" || return 1
-
-    local msg=
-    if ((opt_eval)); then
-      msg="$(eval "$@")"
-    elif ((opt_vars)); then
-      for var in $@; do msg="$msg $var=${!var}"; done
-    else
-      msg="$@"
-    fi
-
-    # If delta is longer than 5 seconds, presume we've re-executed .tick via .bashrc, most likely
-    local epoch_ms=$(datetime-epoch-ms) delta=0
-    ((TICK_LAST_MS)) && delta=$((epoch_ms - TICK_LAST_MS))
-    ((delta > 5000)) && epoch_ms=
-    export TICK_LAST_MS=$epoch_ms
-
-    # unindent for [finish]
-    ((${#TICK_INDENT} >= 2)) && [[ "$msg" =~ ^\[(FINISH|finish).+ ]] && export TICK_INDENT="${TICK_INDENT:0:((${#TICK_INDENT}-2))}"
-
-    printf '+%4d %s %12s %s%b\n' $delta "$(datetime-plus-ms 3 '%D %T')" "$sname" "$TICK_INDENT" "$msg" >> ~/.tick.log
-    # printf '%s %s %s%b\n' "$(date +'%D %T')" "$sname" "$TICK_INDENT" "$msg" >> ~/.tick.log
-    if ((TICK_STDOUT)) || ((TICK_STDERR)); then
-      # local tick_line=$(printf '.tick  %s %s %s%b\n' "$(datetime-plus-ms 3 '%T')" "$sname" "$TICK_INDENT" "$msg")
-      local tick_line="$(printf '+%4d %s %12s %s%b' $delta "$(datetime-plus-ms 3 '%D %T')" "$sname" "$TICK_INDENT" "$msg")"
-      ((TICK_STDOUT)) &&  echo "$tick_line"
-      ((TICK_STDERR)) && >&2 echo "$tick_line"
-    fi
-
-    # indent for [start]
-    [[ "$msg" =~ ^\[(START|start).+ ]] && export TICK_INDENT="$TICK_INDENT  "
-    return 0
-  }
-
-    # usage: [ms places] [format]
-  datetime-plus-ms() {
-    local places="${1:-3}" && shift
-    local format="${1:-%D %T}" && shift
-    local ms="$(perl - <<-'EOF'
-      use Time::HiRes qw(time);
-      my $t = time;
-      printf "%06d", ($t - int($t)) * 1000000;
-    EOF
-    )00000"
-    date +"$format.${ms:0:$places}"
-  }
-
-  datetime-epoch-ms() {
-    echo "$(perl - <<-'EOF'
-      use Time::HiRes qw(time);
-      printf "%d", time * 1000;
-    EOF
-    )"
-  }
-
-  .ttail()  { tail $@ ~/.tick.log; }
-  .tless()  { less $@ ~/.tick.log; }
-  .trm()    { rm -v $@ ~/.tick.log; }
-  
-  alias .reload-tick='. ~/.tick.sh'
+# Return true (0) if ~/.tick.enabled or .tick${sname}.enabled exists; e.g., .tick.bashrc.enabled
+.do-tick() {
+  [[ -e ~/.tick.enabled ]] && return 0
+  [[ -z "$1" ]] && return 1
+  [[ -e ~/.tick$1 ]]
 }
-.tick-wrapper
+
+.tick() {
+  local sname= opt_eval= opt_vars=
+  while [[ "$1" =~ ^- ]]; do case "$1" in
+    -s|--script) sname="$2"; shift 2;;
+    -e|--eval) opt_eval=1; shift;;
+    -v|--vars) opt_vars=1; shift;;
+    *) >&2 echo ".tick: $1: invalid option" && return 1;;
+  esac; done
+  .do-tick "$sname" || return 1
+
+  local msg=
+  if ((opt_eval)); then
+    msg="$(eval "$@")"
+  elif ((opt_vars)); then
+    for var in $@; do msg="$msg $var=${!var}"; done
+  else
+    msg="$@"
+  fi
+
+  # If delta is longer than 5 seconds, presume we've re-executed .tick via .bashrc, most likely
+  local epoch_ms=$(datetime-epoch-ms) delta=0
+  ((TICK_LAST_MS)) && delta=$((epoch_ms - TICK_LAST_MS))
+  ((delta > 5000)) && epoch_ms=
+  export TICK_LAST_MS=$epoch_ms
+
+  # unindent for [finish]
+  ((${#TICK_INDENT} >= 2)) && [[ "$msg" =~ ^\[(FINISH|finish).+ ]] && export TICK_INDENT="${TICK_INDENT:0:((${#TICK_INDENT}-2))}"
+
+  printf '+%4d %s %12s %s%b\n' $delta "$(datetime-plus-ms 3 '%D %T')" "$sname" "$TICK_INDENT" "$msg" >> ~/.tick.log
+  # printf '%s %s %s%b\n' "$(date +'%D %T')" "$sname" "$TICK_INDENT" "$msg" >> ~/.tick.log
+  if ((TICK_STDOUT)) || ((TICK_STDERR)); then
+    # local tick_line=$(printf '.tick  %s %s %s%b\n' "$(datetime-plus-ms 3 '%T')" "$sname" "$TICK_INDENT" "$msg")
+    local tick_line="$(printf '+%4d %s %12s %s%b' $delta "$(datetime-plus-ms 3 '%D %T')" "$sname" "$TICK_INDENT" "$msg")"
+    ((TICK_STDOUT)) &&  echo "$tick_line"
+    ((TICK_STDERR)) && >&2 echo "$tick_line"
+  fi
+
+  # indent for [start]
+  [[ "$msg" =~ ^\[(START|start).+ ]] && export TICK_INDENT="$TICK_INDENT  "
+  return 0
+}
+
+  # usage: [ms places] [format]
+datetime-plus-ms() {
+  local places="${1:-3}" && shift
+  local format="${1:-%D %T}" && shift
+  local ms="$(perl - <<-'EOF'
+    use Time::HiRes qw(time);
+    my $t = time;
+    printf "%06d", ($t - int($t)) * 1000000;
+  EOF
+  )00000"
+  date +"$format.${ms:0:$places}"
+}
+
+datetime-epoch-ms() {
+  echo "$(perl - <<-'EOF'
+    use Time::HiRes qw(time);
+    printf "%d", time * 1000;
+  EOF
+  )"
+}
+
+.ticklog-tail()  { tail $@ ~/.tick.log; }
+.ticklog-less()  { less $@ ~/.tick.log; }
+.ticklog-rm()    { rm -v $@ ~/.tick.log; }
+alias .tt='.ticklog-tail'  .tl='.ticklog-less'  .tr='.ticklog-rm'
+
+test-tick() {
+  
+}
+
+alias .reload-tick='. ~/.tick.sh'  .rlt='.reload-tick'
 
 
